bc52f521b84e2a01a283737915034d653aa2a4fc
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2021-05-18 00:44:23 +0200

    Add Anonymous Function

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,13 @@ class MyApp extends StatelessWidget {
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answerQuestion,
+              onPressed: () => print('Answer 2 choosen'),
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: (){
+                print('Answer 3 choosen');
+              },
             ),
           ],
         ),
