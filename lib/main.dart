b20383541a8fc0c01811192e4d7010f0c445a774
amrabdalfatah <amr.abdalfatah.mohamed@gmail.com>
2021-05-18 02:32:07 +0200

    Add Stateful and question.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print('Answer choosen');
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite Animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -18,10 +34,12 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The Question!'),
+            Question(
+              questions[_questionIndex],
+            ),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
@@ -29,7 +47,7 @@ class MyApp extends StatelessWidget {
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: (){
+              onPressed: () {
                 print('Answer 3 choosen');
               },
             ),
